5cd5538afce6943a22fe5068663f362600e013a6
antran1245 <antran1245@gmail.com>
2022-11-18 19:17:42 -0800

    responsive style for home top box

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import Navigation from './components/Navigation';
+import Home from './components/Home';
 import './App.css';
 import './sass/App.scss'
 
 function App() {
   return (
     <Container>
+      <span id="leaf-image"></span>
       <Navigation />
+      <Home/>
     </Container>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,33 @@
+import { Col, Row, Button } from "react-bootstrap";
+import singleCircle from '../assets/shared/desktop/bg-pattern-small-circle.svg'
+import '../sass/home.scss'
+
 export default function Home() {
     return(
         <>
-            
+            <Row id="headerBox" className="peach-box">
+                <Col xs={12} lg={{offset: 1, span: 6}} id="headerText">
+                    <Row>
+                        <Col>
+                            <p className="heading">Award-winning custom designs and digital branding solutions</p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={12} lg={10}>
+                            <p>With over 10 years in the industry, we are experienced in creating fully responsive websites, app design, and engaging brand experiences. Find out more about our services.</p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Button>LEARN MORE</Button>
+                        </Col>
+                    </Row>
+                </Col>
+                <div id="headerImages">
+                    <img src={singleCircle} alt="big single circle" id="singleCircle"/>
+                    <img src={require('../assets/home/desktop/image-hero-phone.png')} alt="phone" id="phone"/>
+                </div>
+            </Row>
         </>
     )
 }
