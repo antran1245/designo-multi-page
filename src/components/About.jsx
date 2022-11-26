4d876b85bea7eb58d0e1268f00f6fd47a688b988
antran1245 <antran1245@gmail.com>
2022-11-26 14:15:44 -0800

    about us page  desktop  style

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,10 @@
 import { Col, Row } from "react-bootstrap";
+import Countries from "./Countries";
+import GetInTouch from "./GetInTouch";
 import circle from '../assets/about/desktop/bg-pattern-hero-about-desktop.svg'
+import twoCircle from '../assets/shared/desktop/bg-pattern-two-circles.svg'
+import threeCircle from '../assets/shared/desktop/bg-pattern-three-circles.svg'
+import leaf from '../assets/shared/desktop/bg-pattern-leaf.svg'
 import '../sass/about.scss'
 
 export default function About() {
@@ -22,14 +27,32 @@ export default function About() {
                     <img src={require('../assets/about/desktop/image-world-class-talent.jpg')} alt="world class talent"/>
                 </Col>
                 <Col md={{span: 5, offset: 1}}>
-                    <p className="heading2">World-class talent</p>
-                    <p>We are a crew of strategists, problem-solvers, and technologists. Every design is thoughtfully crafted from concept to launch, ensuring success in its given market. We are constantly updating our skills in a myriad of platforms.
-                        <br/><br/>
-Our team is multi-disciplinary and we are not merely interested in form — content and meaning are just as important. We give great importance to craftsmanship, service, and prompt delivery. Clients have always been impressed with our high-quality outcomes that encapsulates their brand&#8217;s story and mission.
-                        
-                    </p>
+                    <div>
+                        <p className="heading2">World-class talent</p>
+                        <p>We are a crew of strategists, problem-solvers, and technologists. Every design is thoughtfully crafted from concept to launch, ensuring success in its given market. We are constantly updating our skills in a myriad of platforms.
+                            <br/><br/>
+    Our team is multi-disciplinary and we are not merely interested in form — content and meaning are just as important. We give great importance to craftsmanship, service, and prompt delivery. Clients have always been impressed with our high-quality outcomes that encapsulates their brand&#8217;s story and mission.
+                            
+                        </p>
+                    </div>
+                    <img src={threeCircle} alt="three cirlces" />
                 </Col>
             </Row>
+            <Countries />
+            <img src={leaf} alt="leaf" id="leaf"/>
+            <Row id="realDeal">
+                <Col md={7}>
+                    <div>
+                        <p className="heading2">The real deal</p>
+                        <p>As strategic partners in our clients&#8217; businesses, we are ready to take on any challenge as our own. Solving real problems require empathy and collaboration, and we strive to bring a fresh perspective to every opportunity. We make design and technology more accessible and give you tools to measure success.<br/><br/>
+We are visual storytellers in appealing and captivating ways. By combining business and marketing strategies, we inspire audiences to take action and drive real results.</p>
+                    </div>
+                </Col>
+                <Col md={5}>
+                    <img src={require('../assets/about/desktop/image-real-deal.jpg')} alt="real deal" />
+                </Col>
+            </Row>
+            <GetInTouch />
         </>
     )
 }
---
+++ src/components/Countries.jsx
@@ -0,0 +1,47 @@
+import { Row, Col, Button } from "react-bootstrap";
+import australia from '../assets/shared/desktop/illustration-australia.svg'
+import canada from '../assets/shared/desktop/illustration-canada.svg'
+import unitedKingdom from '../assets/shared/desktop/illustration-united-kingdom.svg'
+import smallCircle from '../assets/shared/desktop/bg-pattern-small-circle.svg'
+import '../sass/countries.scss'
+
+const data = [
+    {
+        "img": canada,
+        "name": "canada"
+    },
+    {
+        "img": australia,
+        "name": "australia"
+    },
+    {
+        "img": unitedKingdom,
+        "name": "united kingdom"
+    }
+]
+export default function Countries() {
+    return(
+        <Row id="countries">
+            {data.map((item, index) => {
+                return <Col key={index}>
+                            <Row>
+                                <Col className="country-icon">
+                                    <img src={smallCircle} alt="small circle" className="circle"/>
+                                    <img src={item.img} alt={item.name} />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <p>{item.name.toUpperCase()}</p>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <Button>SEE LOCATION</Button>
+                                </Col>
+                            </Row>
+                        </Col>
+            })}
+        </Row>
+    )
+}
