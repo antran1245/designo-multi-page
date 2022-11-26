b8a14da120a18f90e421b8ed4c35b183328b09da
antran1245 <antran1245@gmail.com>
2022-11-25 20:58:13 -0800

    about world   class talent box

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
     return(
         <>
             <Row id="aboutUs">
-                <Col>
+                <Col md={7}>
                     <img src={circle} alt="circle" className="circle"/>
                     <div>
                         <p className="heading1">About Us</p>
@@ -15,6 +15,19 @@ export default function About() {
                 </Col>
                 <Col>
                     <img src={require('../assets/about/desktop/image-about-hero.jpg')} alt="about us"/>
+                </Col>
+            </Row>
+            <Row id="worldClassTalent">
+                <Col md={5}>
+                    <img src={require('../assets/about/desktop/image-world-class-talent.jpg')} alt="world class talent"/>
+                </Col>
+                <Col md={{span: 5, offset: 1}}>
+                    <p className="heading2">World-class talent</p>
+                    <p>We are a crew of strategists, problem-solvers, and technologists. Every design is thoughtfully crafted from concept to launch, ensuring success in its given market. We are constantly updating our skills in a myriad of platforms.
+                        <br/><br/>
+Our team is multi-disciplinary and we are not merely interested in form — content and meaning are just as important. We give great importance to craftsmanship, service, and prompt delivery. Clients have always been impressed with our high-quality outcomes that encapsulates their brand&#8217;s story and mission.
+                        
+                    </p>
                 </Col>
             </Row>
         </>
