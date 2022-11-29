e874a855a591b919320eccf8d944aad111a3a898
antran1245 <antran1245@gmail.com>
2022-11-28 21:21:10 -0800

    Contact  Us style for mobile

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Col, Form, Row, Button } from "react-bootstrap";
 import circle from '../assets/contact/desktop/bg-pattern-hero-desktop.svg'
+import mobileCircle from '../assets/contact/mobile/bg-pattern-hero-contact-mobile.svg'
 import error from '../assets/contact/desktop/icon-error.svg'
 import Countries from "./Countries";
 import '../sass/contact.scss'
@@ -20,14 +21,19 @@ const Contact = () => {
     return(
         <>
             <Row id="contact-container">
-                <Col id="contact-text">
-                    <img src={circle} alt="circle"/>
+                <div className="circle-container d-block d-sm-none">
+                    <img src={mobileCircle} alt="mobile circle"/>
+                </div>
+                <Col xs={12} md={8} xl={6} id="contact-text">
+                    <div className="circle-container d-none d-sm-block">
+                        <img src={circle} alt="circle" width={"100%"}/>
+                    </div>
                     <Col md={8}>
                         <p className="heading1">Contact Us</p>
                         <p>Ready to take it to the next level? Let&#8217;s talk about your project or idea and find out how we can help your business grow. If you are looking for unique digital experiences that&#8217;s relatable to your users, drop us a line.</p>
                     </Col>
                 </Col>
-                <Col>
+                <Col xs={12} xl={5}>
                     <Form onSubmit={submitForm}>
                         <Form.Group as={Row}>
                             <Col>
