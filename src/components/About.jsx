f360c531c3ff2ca78ea268a94487dafdb64125cd
antran1245 <antran1245@gmail.com>
2022-11-28 16:04:03 -0800

    about page add two circle to real deal box

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -48,6 +48,7 @@ export default function About() {
                         <p>As strategic partners in our clients&#8217; businesses, we are ready to take on any challenge as our own. Solving real problems require empathy and collaboration, and we strive to bring a fresh perspective to every opportunity. We make design and technology more accessible and give you tools to measure success.<br/><br/>
 We are visual storytellers in appealing and captivating ways. By combining business and marketing strategies, we inspire audiences to take action and drive real results.</p>
                     </div>
+                    <img src={twoCircle} alt="two circles"/>
                 </Col>
                 <Col md={5}>
                     <img src={require('../assets/about/desktop/image-real-deal.jpg')} alt="real deal" />
